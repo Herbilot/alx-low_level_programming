1bf47d73fbb5d06d92ec0ffd79c488bff0454485
herbilot <hermaregie@gmail.com>
2023-02-16 18:02:29 -0800

    print without printf or puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	while (*s)
+	{
+		putchar(*s++);
+	}
+	return (1);
+}
