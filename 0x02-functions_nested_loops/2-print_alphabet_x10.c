28a73e5fd1f81ee9dbd8571d6b3f66e809ac9004
herbilot <hermaregie@gmail.com>
2023-02-21 18:31:24 -0800

    print the alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,7 +6,7 @@
  */
 void print_alphabet_x10(void)
 {
-	int i;
+	int i = 0;
 	char c;
 
 	while (i < 10)
@@ -16,6 +16,6 @@ void print_alphabet_x10(void)
 			putchar(c);
 		}
 		putchar('\n');
-		++i;
+		i++;
 	}
 }
