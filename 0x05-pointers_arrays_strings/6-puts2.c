2198aeede883c4f3e81158562d2ab30b97b12b35
herbilot <hermaregie@gmail.com>
2023-02-27 16:39:52 -0800

    Write a function that prints every other character of a string, starting with the first character, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * puts2 - print the other caractere
+ * @str: parameter
+ */
+void puts2(char *str)
+{
+	int len = 0;
+
+	while (str[len] != 0)
+	{
+		len++;
+	}
+	for (int i = 0; i < len; i += 1)
+	{
+		printf("%c", str[i]);
+	}
+	printf("\n");
+}
